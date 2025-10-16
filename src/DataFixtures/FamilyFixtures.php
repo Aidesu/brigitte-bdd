1a927f7e59be4702d226fa0cfd3907bb7fa98f7a
Aidesu <waoxsan@gmail.com>
2025-10-16 16:17:27 +0200

    Update a0.1.8 Adding fixtures

---
+++ src/DataFixtures/FamilyFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Familiy;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class FamilyFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        for ($i = 0; $i < 10; $i++){
+            $family = new Familiy();
+            $family->setName("Family {$i}");
+            $manager->persist($family);
+        }
+        $manager->flush();
+    }
+}
